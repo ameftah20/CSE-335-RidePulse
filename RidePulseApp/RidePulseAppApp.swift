72c0e6c068a0d3d0fc3ad9fbb748d0c1490a3f80
ahmedmeftah <ahmed.saniad@gmail.com>
2026-02-14 14:15:32 -0700

    Initial Commit

---
+++ RidePulseApp/RidePulseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidePulseAppApp.swift
+//  RidePulseApp
+//
+//  Created by Ahmed Saniad Meftah on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct RidePulseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
